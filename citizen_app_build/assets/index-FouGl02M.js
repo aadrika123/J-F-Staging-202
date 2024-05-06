d16dc2db460aa9c719e1e482f2fb0d1f1949aa27
Talib Hussain <talibplay111@gmail.com>
2024-05-06 11:30:38 +0530

    changes

--- citizen_app_build/assets/index-FouGl02M.js
+++
@@ -1,1 +0,0 @@
-import{j as r,P as e}from"./index-7g7M7zZQ.js";import"./react-USjiWj3t.js";function n(){return r.jsx(e,{title:"Water Tanker",children:r.jsx("div",{children:"Water Tanker"})})}export{n as default};
