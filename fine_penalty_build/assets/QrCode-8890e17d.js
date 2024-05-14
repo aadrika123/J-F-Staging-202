6c9d9ae5c1de45bc376b0bab6942024911aa3579
akshay2s <akshay.aadrika@gmail.com>
2024-05-14 12:49:46 +0530

    changes

---
+++ fine_penalty_build/assets/QrCode-8890e17d.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-d239668e.js";import{Q as i}from"./index-3e842761.js";function a(e){return t.jsx(i,{value:e==null?void 0:e.url,size:e==null?void 0:e.size})}export{a as Q};
--- fine_penalty_build_old/assets/QrCode-6f560dba.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-e38c13c4.js";import{Q as i}from"./index-38b5d8bb.js";function a(e){return t.jsx(i,{value:e==null?void 0:e.url,size:e==null?void 0:e.size})}export{a as Q};
