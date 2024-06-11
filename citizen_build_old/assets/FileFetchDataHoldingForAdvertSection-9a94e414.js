8c3e79a6383c22d297414f55b11f24f1277856d5
akshay2s <akshay.aadrika@gmail.com>
2024-06-11 16:55:24 +0530

    changes

--- citizen_build_old/assets/FileFetchDataHoldingForAdvertSection-9a94e414.js
+++
@@ -1,1 +0,0 @@
-import"./mui-8e804a04.js";import"./router-87042356.js";import{M as o}from"./index-4e794517.js";import"./index-1ad39463.js";o.setAppElement("#root");
--- fine_penalty_build_old/assets/QrCode-ebc1bd17.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-56164ba8.js";import{Q as i}from"./index-ed999e4d.js";function a(e){return t.jsx(i,{value:e==null?void 0:e.url,size:e==null?void 0:e.size})}export{a as Q};
