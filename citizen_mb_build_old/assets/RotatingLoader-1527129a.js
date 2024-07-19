44c81c2df35fac6ddeb0b3f606ccb6bb9238d464
akshay2s <akshay.aadrika@gmail.com>
2024-07-19 11:02:21 +0530

    changes

--- citizen_mb_build_old/assets/RotatingLoader-1527129a.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-0e94f450.js";import{R as e}from"./Hourglass-60182635.js";function i({}){return t("div",{className:"flex justify-center items-center h-screen",children:t(e,{strokeColor:"#000",width:35,strokeWidth:"3"})})}export{i as R};
--- citizen_mb_build_old/assets/WaterApplicationPayment-92053889.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-0e94f450.js";const e=()=>t("div",{children:"WaterApplicationPayment"});export{e as default};
