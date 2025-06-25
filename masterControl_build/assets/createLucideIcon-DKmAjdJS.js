bb6c9852d5c5f494385501e4a796b4f44b21db9d
Chandankumar728 <chandankumar6299068@gmail.com>
2025-06-25 11:54:20 +0530

    new changes

--- masterControl_build/assets/createLucideIcon-DKmAjdJS.js
+++ masterControl_build/assets/createLucideIcon-DKmAjdJS.js
@@ -1,4 +1,4 @@
-import{r as n}from"./index-CN_yBJeq.js";/**
+import{r as n}from"./index-CE0lAO7R.js";/**
  * @license lucide-react v0.428.0 - ISC
  *
  * This source code is licensed under the ISC license.
---
+++ masterControl_build/assets/index-BY9ERpPG.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CE0lAO7R.js";import{a as e}from"./index-C1-MkYab.js";function s(){return t.jsx("div",{})}function a(){return t.jsx(e,{title:"Menu Role List (master)",subTitle:"",children:t.jsx("div",{className:"py-0",children:t.jsx(s,{})})})}export{a as default};
