963c07c23b63682dfc018cb3d0d9b033a934988c
Chandankumar728 <chandankumar6299068@gmail.com>
2025-09-09 12:35:35 +0530

    new changes

---
+++ citizen_build/assets/pending-3ed0cdd6.js
@@ -0,0 +1,1 @@
+const n="/assets/pending-daaf9508.png";export{n as i};
--- citizen_build_old/assets/FileFetchDataHoldingForAdvertSection-388efe9c.js
+++
@@ -1,1 +0,0 @@
-import"./mui-8bd1858b.js";import"./router-56e993c5.js";import{M as o}from"./index-48939ba9.js";o.setAppElement("#root");
