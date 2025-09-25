cf799f98e895e7071e3a63bcfdbea6341997cdff
Chandankumar728 <chandankumar6299068@gmail.com>
2025-09-25 17:36:53 +0530

    new changes

---
+++ citizen_mb_build/assets/useSetTitle-7980425a.js
@@ -0,0 +1,1 @@
+import{R as s,k as o,r as a}from"./index-7d6593b7.js";function c(t){const{settitleText:e}=s.useContext(o);a.useEffect(()=>{e(t)},[])}export{c as u};
--- citizen_mb_build_old/assets/WaterApplicationPayment-51688f82.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-1b890fc2.js";const e=()=>t("div",{children:"WaterApplicationPayment"});export{e as default};
